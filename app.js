2b75a5abcc9efb9c48f408cfe94ada68123d63d6
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2022-12-27 11:01:50 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(bodyParser({ extended: true }));
+
+module.exports = app;
